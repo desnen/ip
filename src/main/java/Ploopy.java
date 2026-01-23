c2ab9978ba7d95a7f38a22bf865630bac03e7278
Desnen Toh <desnentoh@Desnens-MacBook-Pro.local>
2026-01-23 14:42:24 +0800

    Level-0

---
+++ src/main/java/Ploopy.java
@@ -0,0 +1,7 @@
+public class Ploopy {
+    public static void main(String[] args) {
+        String line = "______________________________________";
+        System.out.println("Hello! I'm Ploopy.\nWhat can I do for you?\n" + line +
+                                "\nBye. Hope to see you again soon!\n" + line);
+    }
+}
