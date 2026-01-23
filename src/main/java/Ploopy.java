c15e39f3671a20e62fdf433593062206b95592f4
Desnen Toh <desnentoh@Desnens-MacBook-Pro.local>
2026-01-23 15:21:39 +0800

    Level-0

--- src/main/java/Ploopy.java
+++ src/main/java/Ploopy.java
@@ -1,18 +1,7 @@
-import java.util.Scanner;
-
 public class Ploopy {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         String line = "______________________________________";
-        System.out.println(line + "\n" + "Hello! I'm Ploopy.\nWhat can I do for you?\n" + line);
-        while (true) {
-            String inp = sc.nextLine();
-            if (inp.equals("bye")) {
-                System.out.println(line + "\n" + "Bye. Hope to see you again soon!\n" + line);
-                break;
-            } else {
-                System.out.println(line + "\n" + inp + "\n" + line);
-            }
-        }
+        System.out.println("Hello! I'm Ploopy.\nWhat can I do for you?\n" + line +
+                "\nBye. Hope to see you again soon!\n" + line);
     }
 }
