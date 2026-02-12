d2df6fcc2b543083e20c07c30cdb70a464c69820
Desnen Toh <desnentoh@Desnens-MacBook-Pro.local>
2026-02-12 11:33:35 +0800

    Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,15 +1,19 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String time;
+    protected LocalDate time;
 
     public Deadline(String description, String time) {
         super(description);
-        this.time = time;
+        this.time = LocalDate.parse(time);
     }
 
     @Override
     public String toString() {
-        return "[D][" + getStatusIcon() + "] " + description + " (by: " + time + ")";
+        return "[D][" + getStatusIcon() + "] " + description + " (by: "
+                        + time.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 
     @Override
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,18 +1,22 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String start;
-    protected String end;
+    protected LocalDate start;
+    protected LocalDate end;
 
     public Event(String description, String start, String end) {
         super(description);
-        this.start = start;
-        this.end = end;
+        this.start = LocalDate.parse(start);
+        this.end = LocalDate.parse(end);
     }
 
     @Override
     public String toString() {
         return "[E][" + getStatusIcon() + "] " + description
-                + " (from: " + start + " to: " + end + ")";
+                        + " (from: " + start.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " to: "
+                        + end.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 
     @Override
